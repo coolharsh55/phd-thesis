aba84afb32af26fcbf02b2d9b4c06405ffd4e683
Harshvardhan Pandit <me@harshp.com>
2020-02-12 09:32:25 +0000

    added evaluation summary table

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 arg=${1:-.}
-exts="aux bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz xml"
+exts="aux bbl blg brf fls fdb_latexmk idx ilg ind lof log lol lot out toc synctex.gz xml"
 
 if [ -d $arg ]; then
     for ext in $exts; do
